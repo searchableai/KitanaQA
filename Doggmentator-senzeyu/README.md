74410b607dac75c6f2d98ce2802d25f6d22711b6
senzeyu <31408450+senzeyu@users.noreply.github.com>
2020-07-01 09:45:16 -0400

    Add files via upload

---
+++ Doggmentator-senzeyu/README.md
@@ -0,0 +1,23 @@
+Doggmentator
+
+- Synonym Replacement (SR) https://arxiv.org/pdf/1603.00892.pdf
+- Random Insertion (RI)
+- Random Swap (RS)
+- Random Deletion (RD)
+- Random Misspelling (RM)
+- Query Reformulation (QR)
+
+
+Install Instructions:
+- Ensure Java v8 is set by default for pyspark compatibility:
+```sudo apt install openjdk-8-jdk```
+```sudo update-alternatives --config java```
+```java -version```
+
+- Setup word vectors
+Unzip word vec files in /support directory
+
+- Install the package
+```python setup.py install```
+
+
---
+++ Doggmentator-senzeyu/setup.py
@@ -0,0 +1,33 @@
+import shutil
+from pathlib import Path
+from setuptools import setup, find_packages 
+ 
+setup(
+    name='Doggmentator',
+    url='https://github.com/searchableai/doggmentator',
+    author='Senzeyu Zhang, Abijith Vijayendra, Aaron Sisto',
+    author_email='aaron@searchable.ai',
+    version='0.1',
+    package_dir={"":"src"},
+    python_requires=">=3.5.0",
+    packages=find_packages("src"),
+    install_requires=[
+        'config',
+        'numpy',
+        'nltk',
+        'stop-words',
+        'pyspark==2.4.0',
+        'spark-nlp==2.5.2'
+    ],
+    extras_require={
+        'dev': [
+            'pytest',
+            'pytest-pep8',
+            'pytest-cov',
+        ]
+    },
+    description='Data augmentation for language models',
+    package_data={
+        'doggmentator':['support/*.txt','support/*.json']
+    },
+)
