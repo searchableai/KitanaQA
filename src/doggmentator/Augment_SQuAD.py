42d38f13dcc913264494fead083726fc23823794
Ubuntu <4207423+mr-snstr@users.noreply.github.com>
2020-07-08 07:01:42 +0000

    Added SQuAD dataset class

--- src/doggmentator/Augment_SQuAD.py
+++ src/doggmentator/Augment_SQuAD.py
@@ -1,7 +1,9 @@
 import pickle
+import hashlib
 import sys
 from torch.utils.data import Dataset
 import torch
+import copy
 from torch.utils.data import DataLoader
 import math
 from collections import Counter
@@ -9,11 +11,187 @@ from doggmentator.drop_words import dropwords
 from doggmentator.term_replacement import *
 import logging
 logger = logging.getLogger().setLevel('INFO')
+from datetime import datetime
 
 data_file = pkg_resources.resource_filename(
-            'doggmentator', 'support/SQuAD_v1.1_dev.pickle')
+            'doggmentator', 'support/squad_bert_importance_scores.pkl')
 with open(data_file, 'rb') as f:
     score_dict = pickle.load(f)
+
+def format_squad(
+        examples: Dict,
+        title_map: Dict,
+        context_map: Dict,
+        version: str='1.1') -> Dict:
+    """ Convert a flat list of dicts to nested SQuAD format """
+    squad_data = {}
+    squad_data['version'] = version
+    squad_data['data'] = []
+    qas = []
+    current_tle_id = None
+    ctx_ids = []
+    tle_ids = []
+    graphs = []
+    num_examples = len(examples)
+    unique_ids = [hashlib.md5(datetime.now().strftime('%Y%m%d%M%s').encode('utf-8')).hexdigest() for x in range(num_examples)]
+
+    dataset = {}
+    for example in examples:
+        qid = example['id']
+        ctx_id = example['ctx_id']
+        tle_id = example['tle_id']
+        if tle_id not in dataset:
+            dataset[tle_id] = {}
+        if ctx_id not in dataset[tle_id]:
+            dataset[tle_id][ctx_id] = []
+        dataset[tle_id][ctx_id].append({
+            'answers':example['answers'],
+            'question':example['question'],
+            'orig_id':qid,
+            'id':qid+unique_ids.pop(),
+            'aug_type':example['aug_type']
+        })
+
+    formatted = {'version':version,'data':[]}
+    for k,v in dataset.items():
+        graphs = []
+        for j,q in v.items(): 
+            graphs.append({
+                'context':context_map[j],
+                'qas':q
+            })
+        formatted['data'].append({
+            'title':title_map[k],
+            'paragraphs':graphs
+        })
+    return formatted
+            
+
+class MySQuADDataset(Dataset):
+    def __init__(self, raw_examples, importance_score_dict=None, is_training=False, sample_ratio=0.5,
+                 p_replace=0.1, p_dropword=0.1, p_misspelling=0.1):
+        # A list of tuples or tensors
+        aug_dataset = []
+        aug_seqs = []
+        examples = []
+        title_map = {}
+        context_map = {}
+        ctx_id = 0
+        for j,psg in enumerate(raw_examples['data']):
+            title = psg['title']
+            tle_id = j
+            title_map[tle_id] = title
+            for n,para in enumerate(psg['paragraphs']):
+                context = para['context']
+                context_map[ctx_id] = context
+                for qa in para['qas']:
+                    examples.append({
+                        'qid':qa['id'],
+                        'ctx_id':ctx_id,
+                        'tle_id':tle_id,
+                        'answers':qa['answers'],
+                        'question':qa['question']
+                    })
+                ctx_id += 1
+        aug_examples = copy.deepcopy(examples)
+
+        # Normalize probabilities of each augmentation
+        probs = [p_dropword, p_replace, p_misspelling]
+        probs = [p / sum(probs) for p in probs]
+        augmentation_types = {
+            'drop': dropwords,
+            'synonym': ReplaceTerms(rep_type='synonym').replace_terms,
+            'misspelling': ReplaceTerms(rep_type='misspelling').replace_terms
+        }
+        num_examples = len(examples)
+        num_aug_examples = math.ceil(num_examples * sample_ratio)
+        orig_indices = list(range(num_examples))
+
+        # Randomly sample indices of data in original dataset with replacement
+        aug_indices = np.random.choice(orig_indices, size=num_aug_examples)
+        aug_freqs = Counter(aug_indices)
+        print(examples, num_examples, num_aug_examples, orig_indices, aug_indices, aug_indices)
+
+        # Reamining number of each agumentation types after exhausting previous example's variations
+        remaining_count = {}
+        for aug_type in augmentation_types.keys():
+            remaining_count[aug_type] = 0
+
+        ct = 0
+
+        for aug_idx, count in aug_freqs.items():
+            # Get frequency of each augmentation type for current example with replacement
+            aug_type_sample = np.random.choice(list(augmentation_types.keys()), size=count, p=probs)
+            aug_type_freq = Counter(aug_type_sample)
+            for aug_type in aug_type_freq.keys():
+                aug_type_freq[aug_type] += remaining_count[aug_type]
+
+            # Get raw data from original dataset and get corresponding importance score
+            raw_data = examples[aug_idx]
+            question = raw_data['question']
+            answers = raw_data['answers']
+            qid = raw_data['qid']
+            ctx_id = raw_data['ctx_id']
+            tle_id = raw_data['tle_id']
+            if importance_score_dict and qid in importance_score_dict:
+                importance_score = importance_score_dict[qid]
+            else:
+                importance_score = None
+
+            if ct % 1000 == 0:
+                print('ct: ', ct)
+                print(question)
+                print(context)
+                print(importance_score)
+                print('+' * 60)
+                print(aug_type_freq)
+
+            for aug_type, aug_times in aug_type_freq.items():
+                if aug_type == 'drop':
+                    aug_questions = augmentation_types[aug_type](question, N = 1, K = 1)
+                else:
+                    aug_questions = augmentation_types[aug_type](sentence = question,
+                                                             importance_scores = importance_score,
+                                                             num_replacements = 1,
+                                                             num_output_sents = aug_times,
+                                                             sampling_strategy = 'topK',
+                                                             sampling_k = 5)
+                #print(question, ' ---- ', aug_questions, '(',aug_type,')')
+                for aug_question in aug_questions:
+                    if is_training:
+                        aug_dataset.append({
+                                                'id':qid,
+                                                'ctx_id':ctx_id,
+                                                'tle_id':tle_id,
+                                                'aug_type':aug_type,
+                                                'question':aug_question,
+                                                'answers':answers,
+                                                'is_impossible':is_impossible
+                                        })
+                    else:
+                        aug_seqs.append({'orig': question, 'aug': aug_question, 'type':aug_type})
+                        aug_dataset.append({
+                                                'id':qid,
+                                                'ctx_id':ctx_id,
+                                                'tle_id':tle_id,
+                                                'aug_type':aug_type,
+                                                'question':aug_question,
+                                                'answers':answers,
+                                        })  
+
+                if ct % 1000 == 0:
+                    print('Generated {} examples'.format(len(aug_dataset)))
+                remaining_count[aug_type] = aug_times - len(aug_questions)
+
+            ct += 1
+
+        formatted_aug_dataset = format_squad(aug_dataset, title_map, context_map)
+        print('Saving data')
+        with open('aug_seqs.json', 'w') as f:
+            json.dump(aug_seqs, f)
+        with open('aug_squad_v1.json', 'w') as f:
+            json.dump(formatted_aug_dataset, f)
+
 
 class MyTensorDataset(Dataset):
     def __init__(self, raw_dataset, tokenizer, importance_score_dict, is_training=False, sample_ratio=0.5,
@@ -138,6 +316,7 @@ class MyTensorDataset(Dataset):
 
 # Load SQuAD Dataset
 from transformers.data.processors.squad import SquadResult, SquadV1Processor, SquadV2Processor, squad_convert_examples_to_features
+'''
 import tensorflow_datasets as tfds
 tfds_examples = tfds.load("squad")
 examples = SquadV1Processor().get_examples_from_dataset(tfds_examples, evaluate=True)
@@ -153,5 +332,11 @@ features, raw_dataset = squad_convert_examples_to_features(
             return_dataset="pt",
             #threads=args.threads,
 )
+'''
 
-dataset = MyTensorDataset(raw_dataset, tokenizer, score_dict)
+#dataset = MyTensorDataset(raw_dataset, tokenizer, score_dict)
+fname = '/home/ubuntu/dev/bootcamp/finetune/SQuAD/eval/support/dev-v1.1.json'
+with open(fname, 'r') as f:
+    data = json.load(f)
+#dataset = MyQADataset(data, score_dict)
+dataset = MySQuADDataset(data)
